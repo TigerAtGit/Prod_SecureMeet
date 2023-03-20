2fb25883f525d88af7cb740edf406457edb42f61
Piyush <77017010+TigerAtGit@users.noreply.github.com>
2023-03-20 22:53:28 +0530

    Revert "Add port for backend"
    This reverts commit f6a484add9503f876db90b11b358099a307fd432.

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,3 @@
-module.exports.BACKEND_URL = "https://securemeet-backend.onrender.com:8080";
+module.exports.BACKEND_URL = "https://securemeet-backend.onrender.com";
 module.exports.FLASK_URL = "http://127.0.0.1:5000";
 module.exports.IP_URL = "https://checkip.amazonaws.com/"
